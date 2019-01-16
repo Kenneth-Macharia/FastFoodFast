d0000657c2ebb84e396b5d4c81f4ec4d6483e66a
Kenneth Macharia <kenneth.macharia@andbeyond.com>
2019-01-16 13:43:10 +0300

    [Chore #163186891] add coveralls and cig

--- app/tests/api_v1/test_config.py
+++ app/tests/api_v1/test_config.py
@@ -1,11 +1,10 @@
 ''' This module contains test configuration setup '''
-import os
 import pytest
 from instance import create_app
 
 @pytest.fixture
 def test_client():
     ''' The test client that will simulate the app behaviour to test '''
-    app = create_app(os.getenv('APP_SETTINGS'))
+    app = create_app('testing')
     return app.test_client()
     
