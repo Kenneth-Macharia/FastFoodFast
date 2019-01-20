ea623156e6f239317d33a1657adfd7e3459a267b
Kenneth Macharia <Kenneth@Kenneths-MacBook-Pro.local>
2019-01-20 17:46:33 +0300

    [Chore #163186891] update requirements.txt

--- app/tests/api_v1/test_app.py
+++ app/tests/api_v1/test_app.py
@@ -7,5 +7,5 @@ from app.tests.api_v1.test_config import test_client
 def test_testapp(test_client):
     '''Tests the GET '/' test endpoint '''
 
-    test_response = test_client.get('/')
+    test_response = test_client.get('/test_api')
     assert 'Hello world' in json.loads(test_response.data)['data']
