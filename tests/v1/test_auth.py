73fc85665a3b490fa02f211a2474232a117131fc
Kenneth Macharia <Kenneth@Kenneths-MacBook-Pro.local>
2019-02-06 07:25:48 +0300

    [Chore #163191112] adds auth tests

--- tests/v1/test_auth.py
+++ tests/v1/test_auth.py
@@ -7,18 +7,40 @@ from configs import test_client, drop_tables
 def test_user_registration(test_client):
     ''' Test the user registration process '''
 
+    # Ensure there are no menus in the database for this test run
+    drop_tables('users')
+
     # Test all user data is entered
+    new_user = {"User_Password":"abc",
+                "User_Name":"Ken"}
+
+    test_response = test_client.post('/v1/auth/signup', data=json.dumps                                          (new_user),                                                                 content_type='application/json')                  
+    assert 'This field cant be left blank!' in json.loads(test_response.data)['message']['User_Email']
 
     # Test user data entered is str
 
     # Test new user creation
+    new_user = {"User_Email":"ken@abc.com",
+                "User_Password":"abc",
+                "User_Name":"Ken"}
+
+    test_response = test_client.post('/v1/auth/signup', data=json.dumps                                          (new_user),                                                                 content_type='application/json')
+
+    assert test_response.status_code == 201
+    assert 'Succesfully signed up' in json.loads(test_response.data)['Response']
 
     # Test same user cannot be registered twice
+    test_response = test_client.post('/v1/auth/signup', data=json.dumps                                          (new_user),                                                                 content_type='application/json')
+
+    assert test_response.status_code == 400
+    assert 'You are already registered' in json.loads(test_response.data)                                                         ['Response']
 
     # Test new/default user is 'Guest' not 'Admin'
 
 
 def test_user_login(test_client):
+    ''' Test the user login process '''
+    pass
     
     # Test all user data is entered
 
@@ -32,6 +54,8 @@ def test_user_login(test_client):
 
 
 def test_user_type_upgrade(test_client):
+    ''' Test the user upgrade process '''
+    pass
     
     # Test all user data is entered
 
@@ -45,6 +69,8 @@ def test_user_type_upgrade(test_client):
 
 
 def test_user_logout(test_client):
+    ''' Test the user logout process '''
+    pass
     
     # Test that a non-logged in user can logout
 
@@ -57,15 +83,10 @@ def test_user_logout(test_client):
 
 
 
-# drop_tables('users')
-#  # Sign up a new user, will be a default 'Guest' user
-#     new_user = {"User_Email":"ken@abc.com",
-#                 "User_Password":"abc",
-#                 "User_Name":"ken"}
 
-#     test_response = test_client.post('/v1/auth/signup', data=json.dumps                                          (new_user),                                                                 content_type='application/json')
+                     
 
-#     assert test_response.status_code == 201
+    
 
 #     # Login the registered user
 #     test_response = test_client.post('/v1/auth/login', data=json.dumps                                           (new_user),                                                                 content_type='application/json')
--- tests/v1/test_menus.py
+++ tests/v1/test_menus.py
@@ -7,10 +7,32 @@ from configs import test_client, drop_tables
 def login_helper(test_client):
     ''' This function will login a registered user '''
 
-    # This user must exist in the database for this test suite
+    # Sign up a user
+    new_user = {"User_Email":"ken@abc.com",
+                "User_Password":"abc",
+                "User_Name":"Ken"}
+
+    test_client.post('/v1/auth/signup', data=json.dumps                                          (new_user),                                                                 content_type='application/json') 
+
+    # Sign in the new user
     new_user = {"User_Email":"ken@abc.com",
                 "User_Password":"abc"}
 
+    test_response = test_client.post('/v1/auth/login', data=json.dumps                                           (new_user),                                                                 content_type='application/json')
+
+    token_data = dict(Authorization="Bearer " + json.loads(test_response.data)["Access_token"])
+
+    # Upgrade new user to 'Admin' status
+    user_to_update = {"User_Email":"ken@abc.com", "User_Type":"Admin"}
+
+    test_client.put('/v1/auth/update', data=json.dumps(user_to_update),
+                    headers=token_data,
+                    content_type='application/json')
+    
+    # Sign out user to revoke 'Guest' token
+    test_response = test_client.post('/v1/auth/logout', headers=token_data)
+
+    # Login again to acquire an 'Admin' token
     test_response = test_client.post('/v1/auth/login', data=json.dumps                                           (new_user),                                                                 content_type='application/json')
 
     token_data = dict(Authorization="Bearer " + json.loads(test_response.data)["Access_token"])
@@ -20,7 +42,7 @@ def login_helper(test_client):
 def test_menus_get(test_client):
     ''' Tests the menus GET ALL '/v1/menus' test endpoint '''
 
-    # Ensure the test database is clean for this test run
+    # Ensure there are no menus in the database for this test run
     drop_tables('menus')
 
     # Login user
@@ -44,18 +66,18 @@ def test_menu_post(test_client):
                         "Menu_ImageURL":"C:/website/menus/images/a_pumkin_soup.jpg",
                         "Menu_Price":20}
 
-    test_response_post = test_client.post('/v1/menu', data=json.dumps                                                 (Menu_item_to_add),
+    test_response = test_client.post('/v1/menu', data=json.dumps                                                 (Menu_item_to_add),
                                            headers=token_data,                  content_type='application/json')
     # Test POST responses
-    assert 'Menu item succesfully added' in json.loads(test_response_post.data)                                                    ['Response']
-    assert test_response_post.status_code == 201
+    assert 'Menu item succesfully added' in json.loads(test_response.data)                                                    ['Response']
+    assert test_response.status_code == 201
 
     # Test POST effect
-    test_response_get = test_client.get('/v1/menus', headers=token_data)
+    test_response = test_client.get('/v1/menus', headers=token_data)
 
-    assert 'Items found' in json.loads(test_response_get.data)
-    assert 'Autumn pumpkin soup' in json.loads(test_response_get.data)                                                     ['Items found'][0]                                                          ['Menu_Name']
-    assert json.loads(test_response_get.data)['Items found'][0]['Menu_Price'] == 20
+    assert 'Items found' in json.loads(test_response.data)
+    assert 'Autumn pumpkin soup' in json.loads(test_response.data)                                                     ['Items found'][0]                                                          ['Menu_Name']
+    assert json.loads(test_response.data)['Items found'][0]['Menu_Price'] == 20
 
 def test_menu_put(test_client):
     ''' Tests the menus PUT '/v1/menu/<menu_id>' test endpoint '''
@@ -69,22 +91,22 @@ def test_menu_put(test_client):
 
     # Tests to verify update change from default {'Unavailable' to 'Available and vice versa}
     # Verify the item created in POST above has the default status
-    test_response_get = test_client.get('/v1/menus', headers=token_data)
+    test_response = test_client.get('/v1/menus', headers=token_data)
 
-    assert 'Unavailable' in json.loads(test_response_get.data)['Items found'][0]                                   ['Menu_Availability']
+    assert 'Unavailable' in json.loads(test_response.data)['Items found'][0]                                   ['Menu_Availability']
     # Perform an update
     status_update = {"Menu_Availability":"Available"}
 
-    test_response_put = test_client.put('/v1/menu/1', data=json.dumps                                               (status_update),
+    test_response = test_client.put('/v1/menu/1', data=json.dumps                                               (status_update),
                                         headers=token_data,                  content_type='application/json')
     
     # Test PUT responses
-    assert 'Menu item updated' in json.loads(test_response_put.data)['Response']
-    assert test_response_put.status_code == 200
+    assert 'Menu item updated' in json.loads(test_response.data)['Response']
+    assert test_response.status_code == 200
 
     # Test PUT effect
-    test_response_get = test_client.get('/v1/menus', headers=token_data)
-    assert 'Available' in json.loads(test_response_get.data)['Items found'][0]                                   ['Menu_Availability']
+    test_response = test_client.get('/v1/menus', headers=token_data)
+    assert 'Available' in json.loads(test_response.data)['Items found'][0]                                   ['Menu_Availability']
 
     # Perform a reverse update
     status_update = {"Menu_Availability":"Unavailable"}
@@ -92,8 +114,8 @@ def test_menu_put(test_client):
     test_client.put('/v1/menu/1', data=json.dumps(status_update),                               headers=token_data, content_type='application/json')
 
     # Test reverse PUT effect
-    test_response_get = test_client.get('/v1/menus', headers=token_data)
-    assert 'Unavailable' in json.loads(test_response_get.data)['Items found'][0]                                      ['Menu_Availability']
+    test_response = test_client.get('/v1/menus', headers=token_data)
+    assert 'Unavailable' in json.loads(test_response.data)['Items found'][0]                                      ['Menu_Availability']
 
 def test_menu_delete(test_client):
     ''' Tests the menus DELETE '/v1/menu/<Menu_Id>' test endpoint '''
@@ -102,14 +124,14 @@ def test_menu_delete(test_client):
     token_data = login_helper(test_client)
 
     # Perform a delete
-    test_response_delete = test_client.delete('/v1/menu/1', headers=token_data)
+    test_response = test_client.delete('/v1/menu/1', headers=token_data)
     
     # Confirm DELETE responses
-    assert 'Menu item deleted' in json.loads(test_response_delete.data)                                                  ['Response']
-    assert test_response_delete.status_code == 200
+    assert 'Menu item deleted' in json.loads(test_response.data)                                                  ['Response']
+    assert test_response.status_code == 200
 
     # Test DELETE effect
-    test_response_get = test_client.get('/v1/menus', headers=token_data)
-    assert 'No items found' in json.loads(test_response_get.data)
-    assert test_response_get.status_code == 200
+    test_response = test_client.get('/v1/menus', headers=token_data)
+    assert 'No items found' in json.loads(test_response.data)
+    assert test_response.status_code == 200
     
